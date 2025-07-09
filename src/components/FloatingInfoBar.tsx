2cb208eee209e261f7297cde81a160609e8e0364
[dyad] <git@dyad.sh>
2025-07-10 00:43:04 +0200

    Updated src/components/FloatingInfoBar.tsx

--- src/components/FloatingInfoBar.tsx
+++ src/components/FloatingInfoBar.tsx
@@ -6,7 +6,7 @@ interface FloatingInfoBarProps {
 
 const FloatingInfoBar = ({ playerCount }: FloatingInfoBarProps) => {
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-20 w-[90vw] sm:w-auto">
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-20 w-[100vw] sm:w-auto">
       <div className="bg-white/80 backdrop-blur-sm border-2 border-black rounded-none shadow-[4px_4px_0px_#000] px-4 py-2 w-full">
         <div className="flex items-center justify-center sm:justify-start space-x-3">
           <Users className="w-5 h-5 text-black" />
