250430421f249005a6cd22dc1433c42d7db77445
LoneBilly <gaudreew@gmail.com>
2025-07-12 16:57:21 +0200

    [dyad] Correction du clic sur les toasts qui fermait les modales - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,6 @@ function App() {
         <Toaster
           position="top-center"
           richColors
-          // closeButton // Supprimé pour utiliser un bouton d'action personnalisé
           className="z-[9999]"
           toastOptions={{
             classNames: {
@@ -95,6 +94,8 @@ function App() {
               closeButton: 'absolute right-2.5 top-2.5 rounded-md p-1 text-white/50 opacity-80 hover:opacity-100 hover:text-white focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-white/50 transition-opacity',
             },
           }}
+          onPointerDown={(e) => e.stopPropagation()}
+          onClick={(e) => e.stopPropagation()}
         />
       </AuthProvider>
     </Router>
