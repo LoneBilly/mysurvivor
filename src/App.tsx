fc4bc39df1a5fa9d4f8c43cfd0bddbff723ab9e8
LoneBilly <gaudreew@gmail.com>
2025-07-12 18:35:50 +0200

    [dyad] Replacing toast close button with an icon - wrote 2 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -85,6 +85,7 @@ function App() {
         <Toaster
           position="top-center"
           richColors
+          closeButton
           className="z-[99999]"
           toastOptions={{
             classNames: {
@@ -93,8 +94,6 @@ function App() {
               description: 'text-xs',
               closeButton: 'absolute right-2.5 top-2.5 rounded-md p-1 text-white/50 opacity-80 hover:opacity-100 hover:text-white focus:opacity-100 focus:outline-none focus:ring-2 focus:ring-white/50 transition-opacity',
             },
-            onMouseDown: (e) => e.stopPropagation(),
-            onClick: (e) => e.stopPropagation(),
           }}
         />
       </AuthProvider>
